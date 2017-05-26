b8291d7996b493447d5425df82f1360c8696863d
Elena Johnson <EJohnson@student.neumont.edu>
2017-05-26 16:44:34 -0600

    Added in the bones of the SelectGame Method

--- CardGameSimulator/CardGameSimulator/RunGames.cs
+++ CardGameSimulator/CardGameSimulator/RunGames.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CSC150_ConsoleMenu;
 
 namespace CardGameSimulator
 {
@@ -10,7 +11,31 @@ namespace CardGameSimulator
     {
         public void SelectGame()
         {
-
+            string[] options = { "Blackjack", "Durak", "Rummy", "War" };
+            bool loop = true;
+            do
+            {
+                int menuSelection = CIO.PromptForMenuSelection(options, true);
+                switch (menuSelection)
+                {
+                    case 1:
+                        //Run Blackjack
+                        break;
+                    case 2:
+                        //run Durak
+                        break;
+                    case 3:
+                        //run rummy
+                        break;
+                    case 4:
+                        //run war
+                        break;
+                    case 0:
+                        Console.WriteLine("Thanks for playing!");
+                        loop = false;
+                        break;
+                }
+            } while (loop);
         }
     }
 }
