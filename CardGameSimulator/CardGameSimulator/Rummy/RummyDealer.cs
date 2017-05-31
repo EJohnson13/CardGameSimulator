e71e55ce524db5d100259a396b796196321e5845
Andrew <andrewsylvester117@gmail.com>
2017-05-30 18:28:57 -0600

    Added my Rummy Dealer and my Rummy Game class

---
+++ CardGameSimulator/CardGameSimulator/Rummy/RummyDealer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameSimulator.Rummy
+{
+    class RummyDealer : Dealer
+    {
+        public override List<Card> CreateDeck()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Card Deal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Shuffle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+        
---
+++ CardGameSimulator/CardGameSimulator/Rummy/RummyGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameSimulator.Rummy
+{
+    class RummyGame : IPlayerable
+    {
+        public List<Card> playerHand { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void AddCard(Card newCard)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Card PlayCard()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
